4b7fb3a95eeeeaecf98fe0653280fb63bd612010
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-01-09 14:36:55 -0500

    Fixed addBookAuthor so that it would still insert a book and not an author if the author already exists. Also refactored my promises to be more readable.

--- dao/bookAuthorDao.js
+++ dao/bookAuthorDao.js
@@ -4,42 +4,90 @@ const dbUtil = require("./dbUtil");
 
 exports.addBookAndAuthor = function(bookAuthor) {
   return new Promise(function(resolve, reject) {
-    let finalResult = {};
-    authorDao
-      .getAuthorByName(bookAuthor.author)
-      .then(function(result) {
-        if (result.length == 0) {
-          authorDao
-            .addAuthor({ name: bookAuthor.author })
-            .then(function(authorResult) {
-              dbUtil.getLastInsertedId().then(function(authorIdObject) {
-                const authorId = authorIdObject[0]["LAST_INSERT_ID()"];
-                bookDao
-                  .addBook({ title: bookAuthor.title, authorId: authorId })
-                  .then(function(bookResult) {
-                    dbUtil.getLastInsertedId().then(function(bookIdObject) {
-                      const bookId = bookIdObject[0]["LAST_INSERT_ID()"];
-                      finalResult = resolve({
-                        bookId: bookId,
-                        authorId: authorId
-                      });
-                    });
-                  })
-                  .catch(function(err) {
-                    return reject(err);
-                  });
-              });
-            })
-            .catch(function(err) {
-              return reject(err);
+    let bookId = 0;
+    let authorId = 0;
+    const getAuthorIdPromise = authorDao.getAuthorByName(bookAuthor.author);
+
+    return (finalResultPromise = getAuthorIdPromise.then(function(authors) {
+      if (authors.length == 0) {
+        const addAuthorPromise = authorDao.addAuthor({
+          name: bookAuthor.author
+        });
+
+        const getAuthorIdPromise = addAuthorPromise
+          .then(function(authorResult) {
+            return dbUtil.getLastInsertedId();
+          })
+          .catch(function(err) {
+            return reject(err);
+          });
+
+        const addBookPromise = getAuthorIdPromise
+          .then(function(authorIdObject) {
+            authorId = authorIdObject[0]["LAST_INSERT_ID()"];
+            return bookDao.addBook({
+              title: bookAuthor.title,
+              authorId: authorId
             });
-        } else {
-          return reject("That author already exists.");
-        }
-      })
-      .catch(function(err) {
-        return reject(err);
-      });
-    return finalResult;
+          })
+          .catch(function(err) {
+            return reject(err);
+          });
+
+        const getBookIdPromise = addBookPromise
+          .then(function(bookResult) {
+            return dbUtil.getLastInsertedId();
+          })
+          .catch(function(err) {
+            return reject(err);
+          });
+
+        const finalPromise = getBookIdPromise
+          .then(function(bookIdObject) {
+            bookId = bookIdObject[0]["LAST_INSERT_ID()"];
+          })
+          .catch(function(err) {
+            return reject(err);
+          });
+
+        Promise.all([
+          getAuthorIdPromise,
+          addAuthorPromise,
+          addBookPromise,
+          getBookIdPromise,
+          finalPromise
+        ]).then(function(result) {
+          return resolve({ bookId: bookId, authorId: authorId });
+        });
+      } else {
+        authorId = authors[0].authorId;
+        const addBookPromise = bookDao.addBook({
+          title: bookAuthor.title,
+          authorId: authorId
+        });
+
+        const getBookIdPromise = addBookPromise
+          .then(function(bookResult) {
+            return dbUtil.getLastInsertedId();
+          })
+          .catch(function(err) {
+            return reject(err);
+          });
+
+        const finalPromise = getBookIdPromise
+          .then(function(bookIdObject) {
+            bookId = bookIdObject[0]["LAST_INSERT_ID()"];
+          })
+          .catch(function(err) {
+            return reject(err);
+          });
+
+        Promise.all([addBookPromise, getBookIdPromise, finalPromise]).then(
+          function(result) {
+            return resolve({ bookId: bookId, authorId: authorId });
+          }
+        );
+      }
+    }));
   });
 };
